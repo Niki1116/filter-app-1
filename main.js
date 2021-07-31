a19d55af7e00afd7c584000db76689ab94efe4a4
Niki1116 <59285462+Niki1116@users.noreply.github.com>
2021-07-31 12:53:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-moustacheX=0; 
-moustacheY=0; 
+noseX=0; 
+noseY=0; 
 function preload() { 
-mustache = loadImage('https://media.istockphoto.com/vectors/black-hipster-vector-mustache-vector-id485318064?k=6&m=485318064&s=170667a&w=0&h=GnKnhEjSZB3_I8CxygnvP7x7AHQcTqLQDXyQYdMTjb4=');
+mustache = loadImage('https://i.postimg.cc/PqgQF9cW/m.png');
 } 
 
 function setup() 
@@ -21,18 +21,17 @@ function setup()
 function gotPoses(results) 
 { if (results.length > 0) { 
  console.log(results); 
- noseX = results[0].pose.moustache.x; 
- noseY = results[0].pose.moustache.y; 
- console.log("moustache x = " + moustacheX); 
- console.log("moustache y = " + moustacheY); 
+ noseX = results[0].pose.nose.x; 
+ noseY = results[0].pose.nose.y; 
+ console.log("moustache x = " + noseX); 
+ console.log("moustache y = " + noseY); 
  } }
 
 function draw() 
 { 
  image(video, 0, 0, 300, 300); 
- fill(255,0,0); 
- stroke(255,0,0); 
- circle(moustacheX, moustacheY, 20);
+image(mustache,noseX-10,noseY+5,30,30);
+
 } 
 function take_snapshot(){ 
  save('myFilterImage.png'); 
